15e7ffecd14d5df96c82c6bd717387b8bf77cbf1
Ravi Kiran Boggavarapu <vsravikiran.b@gmail.com>
2016-03-10 04:07:14 +0530

    ion pre changes. Modified.

--- gray.cpp
+++ gray.cpp
@@ -16,10 +16,8 @@ Mat MaskedFrame;
 Ptr<BackgroundSubtractor> BackgroundSubtractorObject;
 int Keyboard, CarCount=0;
 void ProcessVideo(char* VideoFileName);
-
 int main(int argc, char* argv[])
 {
-<<<<<<< HEAD
   namedWindow("Frame");
   namedWindow("MaskedFrame");
   BackgroundSubtractorObject = createBackgroundSubtractorMOG2();
@@ -27,7 +25,6 @@ int main(int argc, char* argv[])
   destroyAllWindows();
   return EXIT_SUCCESS;
 }
-
 ProcessVideo(char* VideoFileName){
   VideoCapture capture(VideoFileName);
   if(!capture.isOpened()){
@@ -37,56 +34,7 @@ ProcessVideo(char* VideoFileName){
   while ((char)Keyboard!='q' && (char)Keyboard!=27)) {
     if(!capture.read(Frame)){
       cerr << "Unable to read video" <<endl;
-      exit(EXIT_FAILURE);
-=======
-    //create GUI windows
-    namedWindow("Frame");
-    namedWindow("FG Mask MOG 2");
-    //create Background Subtractor objects
-    pMOG2 = createBackgroundSubtractorMOG2(); //MOG2 approach
-        //input data coming from a video
-        processVideo(argv[1]);
-    //destroy GUI windows
-    destroyAllWindows();
-    return EXIT_SUCCESS;
-}
-void processVideo(char* videoFilename) {
-    //create the capture object
-    VideoCapture capture(videoFilename);
-    if(!capture.isOpened()){
-        //error in opening the video input
-        cerr << "Unable to open video file: " << videoFilename << endl;
-        exit(EXIT_FAILURE);
-    }
-    //read input data. ESC or 'q' for quitting
-    while( (char)keyboard != 'q' && (char)keyboard != 27 ){
-        //read the current frame
-        if(!capture.read(frame)) {
-            cerr << "Unable to read next frame." << endl;
-            cerr << "Exiting..." << endl;
-            exit(EXIT_FAILURE);
-        }
-        //update the background model
-        pMOG2->apply(frame, fgMaskMOG2);
-        //get the frame number and write it on the current frame
-        stringstream ss;
-        rectangle(frame, cv::Point(10, 2), cv::Point(100,20),
-                  cv::Scalar(255,255,255), -1);
-        ss << capture.get(CAP_PROP_POS_FRAMES);
-        string frameNumberString = ss.str();
-        putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
-                FONT_HERSHEY_SIMPLEX, 0.5 , cv::Scalar(0,0,0));
-        //show the current frame and the fg masks
-	line(frame,Point(100,150),Point(120,180),Scalar( 0, 255, 0),8,0);
-	if(frameNumberString>"500" && frameNumberString<"550"){
-	Scalar intensity = fgMaskMOG2.at<int>(165, 110); 
-	cout<<endl<<intensity.val[0]<<endl;
-	}
-	imshow("Frame", frame);	
-	imshow("FG Mask MOG 2", fgMaskMOG2);
-        //get the input from the keyboard
-        keyboard = waitKey( 30 );
->>>>>>> origin/master
+      exit(EXIT_FAILURE)
     }
   BackgroundSubtractorObject->apply(Frame, MaskedFrame);
   stringstream ss;
